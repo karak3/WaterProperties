75ff1bb5a8768defecc9ea180a27364c06e4b162
Kara Kelley <77302501+karak3@users.noreply.github.com>
2022-02-26 13:56:24 -0500

    Prototype1

---
+++ WaterPropertiesPrototype1 - Storyline output/story_content/5egBKrgzLEa_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.58.26627.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.970%0D%0AWow!%20That%20name%20is%20awesome!%20It%20reminds%20me%20of%20bananas.%20%0D%0A%0D%0A"}')
