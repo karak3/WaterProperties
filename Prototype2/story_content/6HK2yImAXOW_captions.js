141a8f032925905967cf199168192a5bda2d044d
Kara Kelley <77302501+karak3@users.noreply.github.com>
2022-03-06 17:35:57 -0500

    prototype2

---
+++ Prototype2/story_content/6HK2yImAXOW_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.58.26627.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:01.620%0D%0AClick%20a%20sample%20to%20begin.%20%0D%0A%0D%0A%0D%0A"}')
