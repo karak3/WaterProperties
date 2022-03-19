507bcac3203b38bcf71f008aec63903656084433
Kara Kelley <77302501+karak3@users.noreply.github.com>
2022-03-18 23:25:52 -0400

    DesignProofv2

---
+++ DesignProofv2/story_content/6MHjXaaobLy_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.58.26627.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:01.776%0D%0AStart%20by%20watching%20the%20video.%0D%0A%0D%0A"}')
