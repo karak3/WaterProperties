ef6841ae50546c35dd1a4e514fd9b1023c1f9afb
Kara Kelley <77302501+karak3@users.noreply.github.com>
2022-03-21 11:10:04 -0400

    DesignProofv2

---
+++ DesignProofv2/story_content/6Kh8nsT04nq_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.58.26627.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:02.429%0D%0ADrag%20and%20drop%20an%20ice%20cube%20into%20each%20beaker.%20%0D%0A%0D%0A"}')
