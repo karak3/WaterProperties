ba098da3aa7a2fdd2b55219282287bf02a1c0451
Kara Kelley <77302501+karak3@users.noreply.github.com>
2022-04-17 23:26:24 -0400

    FullGame

---
+++ FullGame/story_content/62M459t3yDX_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.58.26627.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.056%0D%0AObserve%20the%20changes%20and%20record%20the%20ending%20temperatures.%20%0D%0A%0D%0A"}')
---
+++ FullGame/story_content/6iJzNMajQ8Z_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.58.26627.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:04.284%0D%0AYou%20are%20certainly%20not%20dense!%20Good%20work!%20Let\u0027s%20continue%20our%20mission.%20%0D%0A%0D%0A"}')
